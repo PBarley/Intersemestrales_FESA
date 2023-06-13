33c2fb637ad9d66fc425c055706f730ab02cb974
PBarley <pbarron.2002@gmail.com>
2023-06-13 14:32:03 -0600

    Se suben los archivos de la clase del Martes del curso POO con Python3

---
+++ POO_conPython3_Prof.Jesus/Martes/martesPOO_PyCharm/Cosas.py
@@ -0,0 +1,12 @@
+class Alumno:
+
+    facultad = "FES Aragon"
+
+    def __init__(self, nom, ed, carr):
+        self.__nombre = nom
+        self.__edad = ed
+        self.__carrera = carr
+        '''
+        print("constructor")
+        print(type(self))
+        '''
